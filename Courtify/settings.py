08c51ec99b6f6cd7d9cf3f43c964d8c0d106a3c9
tim48-robot <justintimothy303@gmail.com>
2025-10-10 16:17:23 +0700

    feat: INSTALLED_APPS added

--- Courtify/settings.py
+++ Courtify/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication',
-    'manajemen_lapangan',
+    'lapangan',
     'booking_lapangan',
     'main',
 
