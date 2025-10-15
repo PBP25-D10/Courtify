882f445efd717ad661da80ca8faa4f20f31a6ee7
tim48-robot <justintimothy303@gmail.com>
2025-10-15 12:30:53 +0700

    chores: moving the urls, views and base template from manajemen_lapangan

--- lapangan/views.py
+++ lapangan/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from authentication.decorators import role_required, penyedia_required
 
-# Create your views here.
+@penyedia_required
+def manajemen_dashboard_view(request):
+    context = {
+        'user': request.user
+    }
+    return render(request, 'manajemen_lapangan/manajemen_dashboard.html', context)
+
+@penyedia_required
+def lapangan_list_view(request):
+    return HttpResponse("Daftar lapangan milik penyedia")
+
+@penyedia_required
+def lapangan_create_view(request):
+    return HttpResponse("Form tambah lapangan")
+
+@penyedia_required
+def lapangan_edit_view(request, id):
+    return HttpResponse(f"Form edit lapangan ID: {id}")
+
+@penyedia_required
+def lapangan_delete_view(request, id):
+    return HttpResponse(f"Proses hapus lapangan ID: {id}")
