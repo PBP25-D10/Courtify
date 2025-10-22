a358c1d6b95514e714e0c11aa7a2ac29d033ff20
tim48-robot <justintimothy303@gmail.com>
2025-10-22 11:53:09 +0700

    feat: added foreignkey one to many owner to lapangan, migrated

--- lapangan/models.py
+++ lapangan/models.py
@@ -1,10 +1,18 @@
+from django.conf import settings
 from django.db import models
 from django.core.validators import MinValueValidator
-import os 
 import uuid
 # Create your models here.
 
 class Lapangan(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='lapangans',
+        null=True,    
+        blank=True
+    )
+
     id_lapangan = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nama = models.CharField(max_length=100)
     deskripsi = models.CharField(max_length=255)
@@ -21,6 +29,6 @@ class Lapangan(models.Model):
     )
     lokasi = models.CharField(max_length=200)
     harga_per_jam = models.PositiveIntegerField(validators=[MinValueValidator(1)])
-    foto = models.ImageField(upload_to="static/img/")
+    foto = models.ImageField(upload_to='lapangan/%Y/%m/%d/', null=True, blank=True)
     jam_buka = models.TimeField()
     jam_tutup = models.TimeField()
