d3f2ebeadd47a45949cd342a9d53a34d61dd5d4f
rafasoelistiono <rafasoelistiono@gmail.com>
2025-11-24 14:40:40 +0700

    feat:authentication

--- authentication/models.py
+++ authentication/models.py
@@ -11,7 +11,6 @@ class UserProfile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userprofile')
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
-    # ... field lainnya seperti phone, address, dll.
 
     def __str__(self):
         return f"{self.user.username} - {self.get_role_display()}"
